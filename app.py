90d38d1a2099decd5ea4ab770de6739962f0d1d8
Timur Darii <timurdarii95@gmail.com>
2026-02-01 13:33:23 +0000

    feat: implement wallet and user

---
+++ app.py
@@ -0,0 +1,34 @@
+class Wallet:
+    def __init__(self):
+        self.money = 0
+
+    def add_money(self, amount):
+        if amount > 0:
+            self.money += amount
+
+    def withdraw(self, amount):
+        if amount <= self.money:
+            self.money -= amount
+        else:
+            print("Fonduri insuficiente")
+
+
+my_wallet = Wallet()
+my_wallet.add_money(100)
+my_wallet.add_money(50)
+
+# print(my_wallet.money)
+
+
+class User:
+    def __init__(self, name):
+        self.name = name
+        self.wallet = Wallet()
+
+
+user = User('Mihai')
+user.wallet.add_money(100)
+
+w1 = Wallet(100)
+w2 = Wallet(300)
+
